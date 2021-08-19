aa1d00ad3a2531ebfb94174e46a6198f42404dfc
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-19 20:55:11 +0530

    Added:Program to Perform Math Operations Using Lambda Expression

--- MathOperation.java
+++ MathOperation.java
@@ -1,8 +1,20 @@
 package com.interfaceandlambda;
 
+@FunctionalInterface
+interface IMathFunction {
+    int calculate(int a, int b);
+}
+
 public class MathOperation {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Math Operation Program Using " +
-                                "Functional Interface and Lambda Expression");
+        IMathFunction add = (x, y) -> x + y;
+        IMathFunction subtract = (x, y) -> x - y;
+        IMathFunction multiply = (x, y) -> x * y;
+        IMathFunction divide = (x, y) -> x % y;
+
+        System.out.println("Addtion is " + add.calculate(4, 9));
+        System.out.println("Subtraction is " + subtract.calculate(7, 2));
+        System.out.println("Multiplication is " + multiply.calculate(6, 8));
+        System.out.println("Division is " + divide.calculate(3, 9));
     }
 }
