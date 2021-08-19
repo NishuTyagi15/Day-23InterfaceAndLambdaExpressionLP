eb069489fef5ca69dfce3327852560a521d48d4a
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-19 20:45:41 +0530

    Added:Welcome Message

---
+++ MathOperation.java
@@ -0,0 +1,8 @@
+package com.interfaceandlambda;
+
+public class MathOperation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Math Operation Program Using " +
+                                "Functional Interface and Lambda Expression");
+    }
+}
