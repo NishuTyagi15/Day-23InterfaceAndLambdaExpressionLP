9c60c3517a425097de61123196795cc0fd895222
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-19 21:02:47 +0530

    Refactored:Program to Pass Lambda Fuction to Show Result

--- MathOperation.java
+++ MathOperation.java
@@ -3,6 +3,9 @@ package com.interfaceandlambda;
 @FunctionalInterface
 interface IMathFunction {
     int calculate(int a, int b);
+    static void showResult(int a, int b, String function, IMathFunction fnobj ) {
+        System.out.println("Result of " + function + "is " + fnobj.calculate(a, b));
+    }
 }
 
 public class MathOperation {
@@ -16,5 +19,10 @@ public class MathOperation {
         System.out.println("Subtraction is " + subtract.calculate(7, 2));
         System.out.println("Multiplication is " + multiply.calculate(6, 8));
         System.out.println("Division is " + divide.calculate(3, 9));
+
+        IMathFunction.showResult(2, 7, "Addition ", add);
+        IMathFunction.showResult(9, 4, "Subtraction ", subtract);
+        IMathFunction.showResult(6, 8, "Multiplication ", multiply);
+        IMathFunction.showResult(2, 8, "Division ", divide);
     }
 }
